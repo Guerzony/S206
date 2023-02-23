143c91f71219e9353fa9784958b30431bd83b82a
Guerzony <gabrielhenriquecgr2002@gmail.com>
2023-02-22 23:55:35 -0300

    corrigido alunos

--- exercicio1.py
+++ exercicio1.py
@@ -15,10 +15,10 @@ class Aluno:
 
 
 class Aula:
-    def __init__(self, professor, assunto, alunos):
+    def __init__(self, professor, assunto):
         self.professor = professor
         self.assunto = assunto
-        self.alunos = alunos
+        self.alunos = []
 
     def adicionar_aluno(self, aluno):
         self.alunos.append(aluno)
@@ -32,7 +32,7 @@ class Aula:
 professor = Professor("Lucas")
 aluno1 = Aluno("Maria")
 aluno2 = Aluno("Pedro")
-aula = Aula(professor, "Programação Orientada a Objetos", alunos.append(2))
+aula = Aula(professor, "Programação Orientada a Objetos")
 aula.adicionar_aluno(aluno1)
 aula.adicionar_aluno(aluno2)
 print(aula.listar_presenca())
