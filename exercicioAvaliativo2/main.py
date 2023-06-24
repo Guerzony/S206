db24c7f6ac9b84008079b1d453d78463ede44864
Guerzony <gabrielhenriquecgr2002@gmail.com>
2023-06-24 11:22:57 -0300

    Trabalho final de banco de dados

--- exercicioAvaliativo2/main.py
+++ exercicioAvaliativo2/main.py
@@ -1,8 +1,8 @@
 from database import Database
-from TeacherCRUD import BancoDeDados
+from clientConsult import BancoDeDados
 
 # cria uma instância da classe Database, passando os dados de conexão com o banco de dados Neo4j
-db = Database("bolt://3.236.192.75:7687", "neo4j", "decrements-submarined-servants")
+db = Database("bolt://3.89.138.223:76870","neo4j", "cloth-modifications-educators")
 db.drop_all()
 
 # Criando uma instância da classe SchoolDatabase para interagir com o banco de dados
@@ -12,41 +12,60 @@ try:
 except:
     print('Não conectado!')
 
-'''
-#ENTRADA DE DADOS POR USUARIO
+# Criando algumas pessoas
+banco.criar_pessoa("Jogador", "Drake", 31, "M")
+banco.criar_pessoa("Empresario", "Suygetsu", 11, "M")
+banco.criar_pessoa("Lixeiro", "Viserys", 16, "M")
+banco.criar_pessoa("Urologista", "Danny", 63, "M")
+banco.criar_pessoa("Medica", "Lissandra", 32, "F")
+banco.criar_pessoa("Freira", "Dillion", 42, "F")
+banco.criar_pessoa("Psicologa", "Emilia", 56, "F")
+
+# Criando alguns match e suas relações com os alunos
+banco.criar_pet("Leon", 5, "M", "cao")
+banco.criar_pet("Eugenia", 2, "F", "gato")
+banco.criar_pet("Nina", 1, "F", "passaro")
+
+#Criando relacionamento pessoa com pet e pessoa com pessoa
+banco.criar_pessoa_pet("Drake", "Leon")
+banco.criar_pessoa_pet("Drake", "Eugenia")
+banco.criar_pessoa_pet("Drake", "Nina")
+banco.criar_pessoa_pessoa("Viserys", "Drake", "IRMAO_DE")
+banco.criar_pessoa_pessoa("Lissandra", "Suygetsu", "PAI_DE")
+banco.criar_pessoa_pessoa("Lissandra", "Drake", "PAI_DE")
+banco.criar_pessoa_pessoa("Lissandra", "Viserys", "PAI_DE")
+banco.criar_pessoa_pessoa("Danny", "Lissandra", "ESPOSO_DE")
+banco.criar_pessoa_pessoa("Emilia", "Lissandra", "PAI_DE")
+
 while True:
-    print('Oque voce deseja fazer:')
-    print('1- Criar professor')
-    print('2- Atualizar Cpf do professor')
-    print('3- Deletar professor')
-    print('4- Procurar professor')
-    print('5- Sair')
-    escolha = int(input('Escolha:'))
-    if escolha == 1:
-        nome = input('Entre com o nome do professor: ')
-        ano = int(input('Entre com o ano de nascimento do professor: '))
-        cpf = input('Entre com o cpf do professor: ')
-        banco.criar_Teacher(nome, ano, cpf)
+    # Print de todas as informações do banco de dados
+    print("Escolha a opcao")
+    print("0-Mostrar pessoas")
+    print("1-Mostrar pets")
+    print("2-Procurar emprego da pessoa")
+    print("3-Procurar quem é filha de quem")
+    print("4-Procurar quem é casada com outra")
+    print("5-Sair")
+    escolha = int(input("Escolha: "))
+
+    if escolha == 0:
+        print("Pessoas: ", banco.get_pessoa())
+    elif escolha == 1:
+        print("Pets: ", banco.get_pet())
     elif escolha == 2:
-        nome = input('Entre com o nome do professor: ')
-        cpfNovo = input('Entre com o novo cpf: ')
-        banco.update_Teacher(nome, cpfNovo)
+        nome = input("Entre com o nome da profissao: ")
+        print("Quem é " + nome + " :", banco.get_emprego(nome))
     elif escolha == 3:
-        nome = input('Entre com o nome do professor: ')
-        banco.delete_Teacher(nome)
+        nome = input("Entre com o nome do pai ou da mae: ")
+        print("Quem tem " + nome + "como pai ou mae: ", banco.get_Pai(nome))
     elif escolha == 4:
-        nome = input('Entre com o nome do professor: ')
-        print(banco.read_Teacher(nome))
+        nome = input("Entre com o nome da pessoa que voce quer saber do relacionamento: ")
+        print("Essa pessoa possui relacionamento com: ", banco.get_Relacionamento(nome))
     elif escolha == 5:
-        print('Adeus!')
+        print("Adeus!")
         break
     else:
-        print('Opção não existe! Tente novamente!')
-'''
+        print('Opção invalida!')
 
-banco.criar_Teacher('Chris Lima',1956,'189.052.396-66')
-print(banco.read_Teacher('Chris Lima'))
-banco.update_Teacher('Chris Lima', "162.052.777-77")
-
-# Fechando a conexão com o banco
+# Fechando a conexão
 db.close()
